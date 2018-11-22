9aec2358d45b294e66ccd02dfae9d2489e3a7158
David Cull <dcull492@gmail.com>
2018-11-22 11:53:00 +0000

    Stream Updated

--- Streams Exercise/src/streamLesson/Main.java
+++ Streams Exercise/src/streamLesson/Main.java
@@ -1,25 +1,31 @@
 package streamLesson;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main 
 {
-	public static Stream <String> wordCap(List <String> strList)
+	public static List <String> wordCap(List <String> strList)
 	{
 		//List <String> Caps = wordCap(strList);
 		
-		return strList.stream().map(name -> name.toUpperCase());
+			List<String> capitalise = strList.stream()
+				.map(name -> name.toUpperCase())
+				.collect(Collectors.toList());
+			
+			return capitalise;
 	}
 	
 	public static void main(String[] args) 
 	{
 				
-		wordCap(Arrays.asList("Hello", "It's", "Dale"));
-		//System.out.print(wordCap());
+		List<String> listOfStrings = Arrays.asList("Hello", "It's", "Dale");
+		List<String> capitals = wordCap(listOfStrings); 
+		System.out.print(capitals);
 	}
 
 }
