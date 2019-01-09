ba78079aafc1ab7e29decc59ae1e168b04ddfbd2
DCull1984 <nowe2111@yahoo.co.uk>
2019-01-09 09:50:35 +0000

    Update Jenkins_Cent.sh

--- Jenkins_Cent.sh
+++ Jenkins_Cent.sh
@@ -1,3 +1,5 @@
+sudo yum install java-1.8.0-openjdk-devel
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins
